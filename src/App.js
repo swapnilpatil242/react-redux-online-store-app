7cb3e97cbb98717e56abb0ced0cb903019fbac37
swapnilpatil <swapnilpatil242@gmail.com>
2019-04-20 12:43:25 +0530

    Added routing and links

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import OnlineStore from './components/onlineStore/OnlineStore';
+import ManageProduct from './components/productStore/ProductStore';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <OnlineStore />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Route path="/online-store" exact component={OnlineStore}></Route>
+          <Route path="/manage-product" exact component={ManageProduct}></Route>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
   return (
     <div className="Header">
-      <span className="HeaderCommenText">Home</span>
-      <span className="HeaderCommenText">All Products</span>
-      <span className="HeaderCommenText">Products Category</span>
+      <Link to="/" >
+        <span className="HeaderCommenText">Home</span>
+      </Link>
+      <Link to="/manage-product" >
+        <span className="HeaderCommenText">Manage Products</span>
+      </Link>
+      <Link to="/online-store" >
+          <span className="HeaderCommenText">Online Store</span>
+      </Link>
     </div>
   );
 }
